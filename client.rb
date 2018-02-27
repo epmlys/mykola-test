95048d81f87947b5f78245974f31e6f7a0a27864
Mykola Lys <mykolalys@Mykolas-MacBook-Pro.local>
2018-02-27 10:23:38 +0200

    Add CloudHash server and client

--- client.rb
+++ client.rb
@@ -2,24 +2,60 @@
 
 require 'socket'
 
-socket = Socket.new(:INET, :STREAM)
-
-remote_addr = Socket.pack_sockaddr_in(80, 'google.com')
-socket.connect(remote_addr)
-
+#socket = Socket.new(:INET, :STREAM)
+#
+#remote_addr = Socket.pack_sockaddr_in(80, 'google.com')
+#socket.connect(remote_addr)
 # ruby way
-socket = TCPSocket.new('google.com', 80)
-
+#socket = TCPSocket.new('google.com', 80)
 # another ruby way
-Socket.tcp('google.com', 80) do |connection|
-    connection.write "GET / HTTP/1.1\r\n"
-    connection.close
-end
+#Socket.tcp('google.com', 80) do |connection|
+#    connection.write "GET / HTTP/1.1\r\n"
+#    connection.close
+#end
 
 # without block
-client = Socket.tcp('google.com', 80)
+#client = Socket.tcp('google.com', 80)
 
 # write to local server
-client = TCPSocket.new('localhost', 4481)
-client.write('Kolka is cool!')
-client.close
+#client = TCPSocket.new('localhost', 4481)
+#client.write('Kolka is cool!')
+#client.close
+
+module CloudHash
+    class Client
+        class << self
+            attr_accessor :host, :port
+        end
+
+        def self.get(key)
+            request "GET #{key}"
+        end
+
+        def self.set(key, value)
+            request "SET #{key} #{value}"
+        end
+
+        def self.request(string)
+            # create new connection for each operation
+            @client = TCPSocket.new(host, port)
+            @client.write(string)
+
+            # send EOF
+            @client.close_write
+
+            # get response
+            @client.read
+        end
+    end
+end
+
+CloudHash::Client.host = 'localhost'
+CloudHash::Client.port = 4481
+
+puts CloudHash::Client.set('prez', 'Trumph')
+puts CloudHash::Client.get('prez')
+puts CloudHash::Client.get('vp')
+puts CloudHash::Client.set('kolka', 'Cool!')
+puts CloudHash::Client.get('kolka')
+
--- sockets.rb
+++ sockets.rb
@@ -11,23 +11,63 @@ ONE_KB = 1024  # bytes
 print 'Max connections: '
 p Socket::SOMAXCONN
 
-Socket.tcp_server_loop(4481) do |connection|
-    print 'Class: '
-    p connection.class
-    print 'Fileno: '
-    p connection.fileno
-    print 'Local addr: '
-    p connection.local_address
-    print 'Remote addr: '
-    p connection.remote_address
+#Socket.tcp_server_loop(4481) do |connection|
+#    print 'Class: '
+#    p connection.class
+#    print 'Fileno: '
+#    p connection.fileno
+#    print 'Local addr: '
+#    p connection.local_address
+#    print 'Remote addr: '
+#    p connection.remote_address
+#
+#    begin
+#        while data = connection.readpartial(ONE_KB) do
+#            puts data
+#        end
+#    rescue EOFError
+#        print 'EOF!'
+#    end
+#    connection.close
+#end
 
-    begin
-        while data = connection.readpartial(ONE_KB) do
-            puts data
+# Cloud Hash server
+module CloudHash
+    class Server
+        def initialize(port)
+            @server = TCPServer.new(port)
+            puts "Listening on port #{@server.local_address.ip_port}"
+            @storage = {}
         end
-    rescue EOFError
-        print 'EOF!'
-    end
 
-    connection.close
+        def start
+            Socket.accept_loop(@server) do |connection|
+                handle(connection)
+                connection.close
+            end
+        end
+
+        def handle(connection)
+            #read from conn until EOF
+            request = connection.read
+
+            # write back result of processing
+            connection.write process(request)
+        end
+
+        # SET key, GET key
+        def process(request)
+            command, key, value = request.split
+            case command.upcase
+            when 'GET'
+                @storage[key]
+            when 'SET'
+                @storage[key] = value
+                "Has Inserted #{key} - #{value}"
+            end
+        end
+    end
 end
+
+server = CloudHash::Server.new(4481)
+server.start
